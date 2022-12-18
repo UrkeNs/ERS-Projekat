cdcebf7e5ec0448fa038bafe8314c20eb20e25b8
UrkeNs <116072766+UrkeNs@users.noreply.github.com>
2022-12-18 18:51:55 +0100

    Consumer

--- Consumer/Program.cs
+++ Consumer/Program.cs
@@ -3,14 +3,44 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using PowerDistributionSystem;
 
 namespace Consumer
 {
     class Program
     {
+
+
+
         static void Main(string[] args)
         {
-            int a;
+            
+
+            Uredjaj u1 = new Uredjaj("Sporet", 750);
+            Uredjaj u2 = new Uredjaj("Punjac", 40);
+            Uredjaj u3 = new Uredjaj("Mikrotalasna", 400);
+            Uredjaj u4 = new Uredjaj("Fen", 200);
+
+            Console.WriteLine("Odaberite uredjaj koji zelite da upalite: ");
+            string uredjaj = Console.ReadLine();
+
+            if (uredjaj == u1.Naziv1) {
+                dodajUredjaj(u1);
+            }
+
+            
+
+
+
+
+
+
+
+
+
+
+
         }
+    
     }
 }
---
+++ PowerDistributionSystem/Uredjaj.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ServiceModel;
+using System.Runtime.Serialization;
+
+
+namespace PowerDistributionSystem
+{
+    [DataContract]
+    public class Uredjaj
+    {
+        private string Naziv;
+        private int Kwph;
+        private  static int kljucUredjaja = 0;
+
+        Dictionary<int, Uredjaj> RecnikUredjaja = new Dictionary<int, Uredjaj>();
+
+
+        
+        public Uredjaj(string naziv, int kwph)
+        {
+            Naziv = naziv;
+            Kwph = kwph;
+            kljucUredjaja++;
+        }
+
+       /* public void dodajUredjaj(Uredjaj x) {
+            RecnikUredjaja.Add(kljucUredjaja, x);
+        }*/
+
+        public Uredjaj() : this("", 0) { }
+
+        [DataMember]
+        public string Naziv1 { get => Naziv; set => Naziv = value; }
+        [DataMember]
+        public int Kwph1 { get => Kwph; set => Kwph = value; }
+
+
+
+    }
+}

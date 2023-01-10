21733d5c5f768e6ac424a85ce1000ddcd1cbc482
nevena87 <culibrk.nevena@gmail.com>
2023-01-10 17:07:48 +0100

    hidroelectricPowerPlant

---
+++ HydroelectricPowerPlant/HydroelectricPowerPlantServices.cs
@@ -0,0 +1,32 @@
+﻿using DistroHidro;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HydroelectricPowerPlant
+{
+    class HydroelectricPowerPlantServices : IHidroelectricPower
+    {
+        public double PotraznjaZaGenerisanje()
+        {
+            Random rand = new Random();
+
+            double elektricnaEnergija = 0;
+
+            int elektricnaEnergijaProc = 0;
+
+            double maxEnergija = 2000;
+
+            elektricnaEnergijaProc = rand.Next(0, 101);
+
+            elektricnaEnergija = maxEnergija * ((double)elektricnaEnergijaProc / 100);
+
+            Console.WriteLine("Trenutno generisana elektricna energija u procentima: " + elektricnaEnergijaProc + "%");
+            Console.WriteLine("Trenutna elektricna energija u MV: " + elektricnaEnergija + "MV");
+
+            return elektricnaEnergija;
+        }
+    }
+}
---
+++ HydroelectricPowerPlant/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ServiceModel;
+using DistroHidro;
+
+namespace HydroelectricPowerPlant
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (ServiceHost host = new ServiceHost(typeof(HydroelectricPowerPlantServices)))
+            {
+
+                string adresa = "net.tcp://localhost:3998/Hidro";
+                NetTcpBinding binding = new NetTcpBinding();
+                host.AddServiceEndpoint(typeof(IHidroelectricPower), binding, adresa);
+                host.Open();
+                Console.WriteLine("Hidroelektrana je otvorena");
+
+                Console.ReadLine();
+            }
+        }
+    }
+}
